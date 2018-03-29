0e593c0d7eb538d50b163af51109f7145c34b71f
cesarfuster <cfuster@uol.com.br>
2018-03-29 16:07:29 -0300

    Code done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :ingredients, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [ :index, :new, :create ]
+  end
+  resources :doses, only: [:destroy]
+
+  root to: 'cocktails#index'
 end
